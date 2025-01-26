b6e8d5cc2d40823903ad41993e0f5c25c30f655b
Mohanad <112412927+MQHANAD@users.noreply.github.com>
2025-01-26 21:26:37 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://s202024100:xoznaw-hucjuV-gexqo5@cluster0.pfmej.mongodb.net/Sen?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "";
 const mongoose = require("mongoose")
 mongoose.connect(uri)
   .then (()=>console.log("connected"))
